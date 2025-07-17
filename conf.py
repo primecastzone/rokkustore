d3a4d698d890a945e62b0f59408ecd18dd3b17fd
primecastzone <vinitasukheja1@gmail.com>
2025-07-17 16:04:03 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'Disneyplus TV'
+project = 'roku link'
 
 
 # The full version, including alpha/beta/rc tags
